5128f086e0c50003490814f8fc4d1fdc61ccaf7f
MD Nasim <nasimubd21@gmail.com>
2026-09-23 08:23:44 +0800

    test(migration): retain required rsync flags

---
+++ crates/sanctum-runtime/tests/migration_rsync_flags.rs
@@ -0,0 +1,3 @@
+use sanctum_runtime::migration::{MigrationPaths,RsyncInvocation,SyncPass};
+use std::path::PathBuf;
+#[test]fn uses_attribute_flags(){let paths=MigrationPaths::new(PathBuf::from("/Volumes/Origin/data"),PathBuf::from("/Volumes/Target/data")).unwrap();let call=RsyncInvocation::new(PathBuf::from("/opt/homebrew/bin/rsync"),&paths,SyncPass::Initial);assert_eq!(call.arguments[0],"-avXHE");assert_eq!(call.arguments[1],"--");}
