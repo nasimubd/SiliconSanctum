81169b3f46c3e11a35cd87aa468e4edb5936e337
MD Nasim <nasimubd21@gmail.com>
2026-09-23 09:04:16 +0800

    test(migration): cover monitor plist escaping

---
+++ crates/sanctum-runtime/tests/migration_plist_escape.rs
@@ -0,0 +1,9 @@
+use sanctum_runtime::migration::escape_plist_xml;
+
+#[test]
+fn escapes_all_xml_metacharacters() {
+    assert_eq!(
+        escape_plist_xml("&<>\"'"),
+        "&amp;&lt;&gt;&quot;&apos;"
+    );
+}
