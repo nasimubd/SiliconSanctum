62e663cf7a2b7d9c30105c457ec643036f103b83
MD Nasim <nasimubd21@gmail.com>
2026-09-23 08:21:23 +0800

    test(migration): accept supported link speeds

---
+++ crates/sanctum-runtime/tests/migration_link_speed.rs
@@ -0,0 +1,2 @@
+use sanctum_runtime::migration::LinkSpeed;
+#[test]fn accepts_supported_speeds(){assert_eq!(LinkSpeed::parse("8.0 GT/s").unwrap(),LinkSpeed::Gt8);assert_eq!(LinkSpeed::parse("16.0 GT/s").unwrap(),LinkSpeed::Gt16);}
